42056ed32839891507a18b57ebdd8376d474dcd6
Paulo Alenquer <pauloalenquer@gmail.com>
2022-02-22 18:52:35 -0300

    fix: changed logo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { MdOutlineCatchingPokemon } from "react-icons/md";
+import { CgPokemon } from "react-icons/cg";
 
 export default function Header() {
   return (
-    <header className="lg:mx-auto lg:container lg:px-40 md:py-8 p-4 flex items-center gap-4">
-      <MdOutlineCatchingPokemon size={40} />
+    <header className="lg:mx-auto lg:container lg:px-40 md:py-8 p-4 flex items-center gap-2">
+      <CgPokemon size={40} />
 
       <h1 className="font-black text-4xl">Pokédex</h1>
     </header>
