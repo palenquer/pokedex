4b9d31c3fc0764da0ca2eaa6e621ae975fe6163b
Paulo Alenquer <pauloalenquer@gmail.com>
2022-02-23 20:25:57 -0300

    added link to home page in header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
 import React from "react";
 import { CgPokemon } from "react-icons/cg";
 
 export default function Header() {
   return (
-    <header className="lg:mx-auto lg:container lg:px-40 md:py-8 p-4 flex items-center gap-2">
-      <CgPokemon size={40} />
+    <header className="lg:mx-auto lg:container lg:px-40 md:py-8 p-4 ">
+      <Link href="/">
+        <a className="flex items-center gap-2">
+          <CgPokemon size={40} />
 
-      <h1 className="font-black text-4xl">Pokédex</h1>
+          <h1 className="font-black text-4xl">Pokédex</h1>
+        </a>
+      </Link>
     </header>
   );
 }
