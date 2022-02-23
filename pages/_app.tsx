6c4beb8bfec706224b52b1f2e7fabd1732d27e46
Paulo Alenquer <pauloalenquer@gmail.com>
2022-02-23 20:26:13 -0300

    feature: added toastify dependencie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,35 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const contextClass = {
+    success: "bg-blue-600",
+    error: "bg-red-600",
+    info: "bg-gray-600",
+    warning: "bg-orange-400",
+    default: "bg-indigo-600",
+    dark: "bg-white-600 font-gray-300",
+  };
+
   return (
     <div className="font-poppins text-darkgray">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
       <Header />
-      
+
       <Component {...pageProps} />
     </div>
   );
