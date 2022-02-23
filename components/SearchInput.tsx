1806def01eee6cca8f5719edba3beb9009523cce
Paulo Alenquer <pauloalenquer@gmail.com>
2022-02-23 20:25:11 -0300

    feature: search input is working

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
-export default function SearchInput() {
+interface SearchInputProps {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => {};
+  onSubmit: (event: React.FormEvent) => {};
+  value: string;
+}
+
+export default function SearchInput({
+  onChange,
+  value,
+  onSubmit,
+}: SearchInputProps) {
   return (
-    <input
-      className="w-full h-12 p-2 rounded-md placeholder:italic text-gray-800 border-2 border-lightgray"
-      type="text"
-      placeholder="Search"
-    />
+    <form
+      className="relative focus-within:text-darkgray flex gap-4"
+      onSubmit={onSubmit}
+    >
+      <input
+        className="border-2 rounded-md border-lightgray py-3 px-4 bg-white placeholder-mediumgray text-darkgray appearance-none w-full focus:outline-none"
+        type="text"
+        value={value}
+        placeholder="Search for a name or id"
+        onChange={onChange}
+      />
+
+      <button
+        className="bg-lightgray w-14 rounded-md flex justify-center items-center hover:brightness-90"
+        type="submit"
+      >
+        <AiOutlineSearch className="text-mediumgray" size={22} />
+      </button>
+    </form>
   );
 }
