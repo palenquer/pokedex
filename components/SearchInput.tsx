767c60741a37a13ec4b7e35ba302ea08c3ffa634
Paulo Alenquer <pauloalenquer@gmail.com>
2022-02-24 13:18:48 -0300

    added a new button to clear text input

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineClose } from "react-icons/ai";
 
 interface SearchInputProps {
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => {};
-  onSubmit: (event: React.FormEvent) => {};
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (event: React.FormEvent) => void;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   value: string;
 }
 
@@ -11,19 +12,29 @@ export default function SearchInput({
   onChange,
   value,
   onSubmit,
+  onClick,
 }: SearchInputProps) {
   return (
     <form
       className="relative focus-within:text-darkgray flex gap-4"
       onSubmit={onSubmit}
     >
-      <input
-        className="border-2 rounded-md border-lightgray py-3 px-4 bg-white placeholder-mediumgray text-darkgray appearance-none w-full focus:outline-none"
-        type="text"
-        value={value}
-        placeholder="Search for a name or id"
-        onChange={onChange}
-      />
+      <label className="w-full relative">
+        <input
+          className="border-2 rounded-md border-lightgray py-3 px-4 bg-white placeholder-mediumgray text-darkgray appearance-none w-full focus:outline-none"
+          type="text"
+          value={value}
+          placeholder="Search for a name or id"
+          onChange={onChange}
+        />
+        <button
+          className="absolute top-1/2 transform -translate-y-1/2 right-0 h-full w-12 flex items-center justify-center"
+          type="button"
+          onClick={onClick}
+        >
+          <AiOutlineClose className="text-mediumgray" size={20} />
+        </button>
+      </label>
 
       <button
         className="bg-lightgray w-14 rounded-md flex justify-center items-center hover:brightness-90"
